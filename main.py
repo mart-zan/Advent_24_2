5f28966e1896cf40fd8daeccde17fbf08aea6317
Žaneta Martinů <zaneta.martinu@vdimaging.com>
2024-12-10 12:45:22 +0100

    All conditions.

--- main.py
+++ main.py
@@ -9,18 +9,35 @@ def read_reports(filename: str):
     # Returns
     return rows
 
+def all_same_sign(list_numbers):
+    if all(col > 0 for col in list_numbers):
+        return True
+    elif all(col < 0 for col in list_numbers):
+        return True
+    else:
+        return False
+
 
 if __name__ == '__main__':
     # Read and close file
     reports = read_reports("input.txt")
-    print(reports)
+    save_reports = 0
     for i, report in enumerate(reports):
         # report from string to numbers
         report = report.strip().split(' ')
         report = list(map(int, report))  # ineteger from string
-        print(report)
+        # print(report)
         # Keep safe through conditions
-        safe = True
+        # safe = True
         # Make difference to check plus/minus (ascend/descend)
         diffs_report = res = [report[j + 1] - report[j] for j in range(len(report)-1)]
-        print(diffs_report)
+        # print(diffs_report)
+        # safe_sign =
+        # print(safe_sign)
+        if all_same_sign(diffs_report):
+            if any(y > 3 for y in diffs_report):
+                if (all(y != 0 for y in diffs_report)):
+                    save_reports = save_reports + 1
+
+
+    print('In unusual data from the Red-Nosed reactor is ' + str(save_reports) + ' save reports.')
